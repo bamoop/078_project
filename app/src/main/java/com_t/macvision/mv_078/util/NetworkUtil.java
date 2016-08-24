9be1d4ef626bd2d60114f644665e8d630ba49fba
lx.sz <liangxiong.sz@foxmail.com>
2016-08-24 14:55:28 +0800

    更新

--- app/src/main/java/com_t/macvision/mv_078/util/NetworkUtil.java
+++ app/src/main/java/com_t/macvision/mv_078/util/NetworkUtil.java
@@ -2,6 +2,10 @@ package com_t.macvision.mv_078.util;/**
  * Created by bzmoop on 2016/8/24 0024.
  */
 
+import android.content.Context;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
+
 /**
  * 作者：LiangXiong on 2016/8/24 0024 13:43
  * 邮箱：liangxiong.sz@foxmail.com
@@ -9,4 +13,17 @@ package com_t.macvision.mv_078.util;/**
  */
 public class NetworkUtil {
     String s;
+
+    public static String getIP(Context context) {
+        String mIp = null;
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
+        if (dhcpInfo != null && dhcpInfo.gateway != 0) {
+            mIp = ((dhcpInfo.gateway & 0xFF) + "." +
+                    ((dhcpInfo.gateway >>>= 8) & 0xFF) + "." +
+                    ((dhcpInfo.gateway >>>= 8) & 0xFF) + "." +
+                    ((dhcpInfo.gateway >>>= 8) & 0xFF));
+        }
+        return mIp;
+    }
 }
